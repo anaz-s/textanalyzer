6d40586cdf7c295ef9dff6ea4df1c781b72cfeee
anaz-s <samanazodo20@gmail.com>
2024-03-11 22:36:44 +0100

    Initial commit

--- main.go
+++ main.go
@@ -8,17 +8,6 @@ import (
 
 )
 
-/* func createFile(filename string, s string) (error){
-
-	data := []byte(s)
-	err := os.WriteFile(filename, data, 0666)
-	if err != nil {
-		return err
-	}
-	log.Println(data)
-	return nil
-} */
-
 func readaFile(filename string) string{
 
 	data, err := os.ReadFile(filename)
@@ -32,11 +21,10 @@ func countChars(s string) int{
 	return len(s)
 }
 
-func countWords(s string) []string{
+func countWords(s string) int{
 
 	words := strings.Fields(s)
-	return words
-	// return len(words)
+	return len(words)
 
 }
 
@@ -46,29 +34,12 @@ func countLines(s string) int{
 	return len(lines)
 }
 
-func countFrequency(s []string) map[string]int{
-
-	m := make(map[string]int)
-	for _, value := range s{
-		count, ok := m[value]
-		if !ok {
-			count = 1
-		} else {
-			count++
-		}
-		m[value] = 1
-
-	}
-	return m
-}
 
 func main(){
 
 	
-	fmt.Println("Character Length:",countChars(readaFile("/etc/X11/rgb.txt")))
-	fmt.Println("Words Count:",countWords(readaFile("/etc/X11/rgb.txt")))
-	fmt.Println("Line Count:",countLines(readaFile("/etc/X11/rgb.txt")))
-	fmt.Println("Word Frequency:",countFrequency(countWords(readaFile("/etc/X11/rgb.txt"))))
-
+	fmt.Println("Character Length:",countChars(readaFile("/etc/theHarvester/wordlists/dns-big.txt")))
+	fmt.Println("Words Count:",countWords(readaFile("/etc/theHarvester/wordlists/dns-big.txt")))
+	fmt.Println("Line Count:",countLines(readaFile("/etc/theHarvester/wordlists/dns-big.txt")))
 	
 }
